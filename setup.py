12169fbdb156e1c91b42a3b8eabcae8f81cdcdb5
annoma <=>
2010-02-23 21:39:11 +0100

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,16 @@
-from distutils.core import setup
-
-setup(name='django-paypal',
-      version='1.0',
-      zip_safe=False,
+from setuptools import setup, find_packages
+ 
+setup(
+    name='django-paypal',
+    version='1.0',
+    description='PayPal integration into Django.',
+    url='http://github.com/johnboxall/django-paypal',
+    packages=find_packages(),
+    classifiers=[
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ],
+    include_package_data=True,
+    zip_safe=False,
 )
